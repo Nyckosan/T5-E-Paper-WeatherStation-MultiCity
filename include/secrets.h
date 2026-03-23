aaba55def679e6a0385662d9ae052cec38d8f549
Nyckosan <59937513+Nyckosan@users.noreply.github.com>
2026-03-22 21:38:29 -0700

    Add files via upload

---
+++ include/secrets.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Local-only credentials file (ignored by git).
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+#define OWM_API_KEY "YOUR_OPENWEATHERMAP_API_KEY"
